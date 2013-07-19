44cd20c91efbb588779ea4e9c65527e1d9d8c3e0
git <git@git-virtual-machine.(none)>
2013-07-19 18:30:41 +0800

    1

---
+++ app/models/task.rb
@@ -0,0 +1,4 @@
+class Task < ActiveRecord::Base
+  attr_accessible :done, :due, :name, :notes, :priority
+  attr_accessible :done, :due, :name, :notes, :priority
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NetzkeTaskManager::Application.config.secret_token = '46427ba8213d28cb6ca47f5f15ade4bf6bdc0e65df0cae3ff0989817881ee2e71769159e43b2e4678cc79da611cdbeaa3aaf6238f9c65974013e52e467ceb47a'
